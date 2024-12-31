9f221fc9d6ad9a5ff03fed4b67ccfef0ce990442
Bhavishya <bhavishya1102@gmail.com>
2024-12-31 11:36:02 +0530

    implement basic rigid body

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,22 @@
 use ggez::graphics;
-use nalgebra::Vector2;
-use schema::{Move, PhysicsObject};
 
-mod schema;
+mod system;
 
 use ggez::event;
 use ggez::glam::*;
 use ggez::graphics::Color;
+use ggez::graphics::Rect;
 use ggez::{Context, GameResult};
+use system::RigidBody;
 
 struct MainState {
-    objects: Vec<PhysicsObject<f32>>,
+    objects: Vec<RigidBody>,
 }
 
 impl MainState {
     fn new() -> GameResult<MainState> {
         let s = MainState {
-            objects: vec![
-                PhysicsObject::new(Vector2::from([1.2, 2.2]), Vector2::from([1.2, 2.2]), 10.2),
-                PhysicsObject::new(Vector2::from([1.2, 3.2]), Vector2::from([5.2, 2.2]), 9.2),
-            ],
+            objects: vec![RigidBody::new((20.0, 10.0), 10.0)],
         };
         Ok(s)
     }
@@ -30,8 +27,13 @@ impl event::EventHandler<ggez::GameError> for MainState {
         let dt = ctx.time.delta();
 
         for object in &mut self.objects {
-            object.apply_force(Vector2::from([0.0, 5.0]), dt.as_secs_f32());
-            object.apply_velocity(dt.as_secs_f32());
+            if !object.forces.contains_key("force1") {
+                object.add_persistent_force("force1", (0.0, 10.0));
+            }
+            if !object.forces.contains_key("force3") {
+                object.add_persistent_force("force3", (14.0, 10.0));
+            }
+            object.update(dt.as_secs_f32());
         }
         Ok(())
     }
@@ -46,13 +48,16 @@ impl event::EventHandler<ggez::GameError> for MainState {
         canvas.draw(&text, Vec2::new(3.0, 3.0));
 
         for object in &mut self.objects {
-            let circle = graphics::Mesh::new_circle(
+            let circle = graphics::Mesh::new_rectangle(
                 ctx,
                 graphics::DrawMode::fill(),
-                Vec2::new(0.0, 0.0),
-                object.mass * 2.0,
-                0.1,
-                Color::WHITE,
+                Rect {
+                    h: object.mass * 20.0,
+                    w: object.mass * 20.0,
+                    x: object.position.x,
+                    y: object.position.y,
+                },
+                Color::GREEN,
             )?;
             canvas.draw(&circle, Vec2::new(object.position[0], object.position[1]));
         }
@@ -62,7 +67,7 @@ impl event::EventHandler<ggez::GameError> for MainState {
     }
 }
 pub fn main() -> GameResult {
-    let cb = ggez::ContextBuilder::new("super_simple", "ggez");
+    let cb = ggez::ContextBuilder::new("engine", "rpe");
     let (ctx, event_loop) = cb.build()?;
     let state = MainState::new()?;
     event::run(ctx, event_loop, state)
