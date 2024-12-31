f43f7be422cb7f5f1137c656f8b3fe3f930a7bf7
Bhavishya <bhavishya1102@gmail.com>
2024-12-31 11:35:46 +0530

    create a new module —system

--- src/schema/mod.rs
+++
@@ -1,4 +0,0 @@
-mod physics_object;
-// mod physics_system;
-pub use physics_object::*;
-// pub use physics_system::PhysicsSystem;
---
+++ src/system/mod.rs
@@ -0,0 +1,4 @@
+// mod force;
+mod rigid_body;
+
+pub use rigid_body::RigidBody;
